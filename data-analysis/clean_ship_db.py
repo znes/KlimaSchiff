746e5eff5f01e60d0a2b5310a718b08b8da774cc
simnh <simon.hilpert@uni-flensburg.de>
2020-05-19 11:14:04 +0200

    Add clean ship db

---
+++ data-analysis/clean_ship_db.py
@@ -0,0 +1,53 @@
+import os
+
+import pandas as pd
+
+
+path = os.path.join(
+    os.path.expanduser("~"), "nextcloud-znes", "KlimaSchiff", "Data", "Ship_DB"
+)
+
+
+df = pd.read_excel(
+    os.path.join(path, "Ship-DB-TabD-200220.xlsx"),
+    sheet_name="Ship-DB-TabD-200220",
+)
+
+df = df.drop(
+    columns=[
+        "Werftname",
+        "Werftort",
+        "BauNummer",
+        "Ind",
+        "Aus",
+        "Flagge",
+        "Heimathafen",
+        "Status",
+        "US",
+        "Gesellsch",
+        "Betreiber",
+        "WerftTyp",
+        "Kiell",
+        "Aufschw",
+        "Indienst",
+        "Bemerkung",
+        "Werft",
+        "Bau_Nr",
+        "N_Nr",
+        "BRT",
+        "NRT",
+        "TDW",
+        "NRZ",
+    ]
+)
+
+df.drop(
+    df[
+        (df["Schiffstyp"].isna() == True)
+        | (df["Schiffstyp"] == "4")
+        | (df["Schiffstyp"] == " ")
+    ].index,
+    inplace=True,
+)
+
+df.to_csv(os.path.join(path, "schiffsdatenbank.csv"))
