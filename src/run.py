07b4ed31c7779a85d25b311da0e1cb0ca8cb3234
simnh <simon.hilpert@uni-flensburg.de>
2021-11-18 11:05:56 +0100

    Enable mulitprocessing for rasterize

--- src/run.py
+++ src/run.py
@@ -5,13 +5,17 @@ from calculate_routes import calculate_routes
 from calculate_emissions import calculate_emissions, append_additional_emissions_to_lcpa
 from rasterize_points import rasterize_points
 import preprocess as preprocess
+from multiprocessing import Pool
+from itertools import repeat
 
 @click.group()
 @click.option('-p', '--preprocess', type=bool, default=False, help='If raw data needs to be preprocessed first')
+@click.option('--parallel', '-l', type=int, help="Number of processes (int) when using multiprocessing parallel computing")
 @click.pass_context
-def cli(ctx, preprocess):
+def cli(ctx, preprocess, parallel):
     ctx.ensure_object(dict)
     ctx.obj['PREPROCESS'] = preprocess
+    ctx.obj['PARALLEL'] = parallel
 
 @cli.command()
 def reduce():
@@ -40,28 +44,48 @@ def emissions():
     with open("config.json") as file:
         config = json.load(file)
 
-    # create up-to-date model file 
+    # create up-to-date model file
     append_additional_emissions_to_lcpa()
 
     calculate_emissions(config)
 
 @cli.command()
-def rasterize():
+@click.pass_context
+def rasterize(ctx):
     with open("config.json") as file:
         config = json.load(file)
-    rasterize_points(
-        config=config,
-        emission_types={
-        "SOx [kg]": "SO2",
-        "PM [kg]": "PM",
-        "NOx [kg]": "NOx",
-        "CO2 [kg]": "CO2",
-        "BC [kg]": "EC",
-        "ASH [kg]": "Ash",
-        "POA [kg]": "POA",
-        "CO [kg]": "CO",
-        "NMVOC [kg]": "NMVOC",
-    })
+    # parallel
+    if ctx.obj["PARALLEL"] > 0:
+        pool = Pool(processes=ctx.obj["PARALLEL"])
+        pool.starmap(
+            rasterize_points,
+            zip(repeat(config), [
+                {"SOx [kg]": "SO2"},
+                {"PM [kg]": "PM"},
+                {"NOx [kg]": "NOx"},
+                {"CO2 [kg]": "CO2"},
+                {"BC [kg]": "EC"},
+                {"ASH [kg]": "Ash"},
+                {"POA [kg]": "POA"},
+                {"CO [kg]": "CO"},
+                {"NMVOC [kg]": "NMVOC"}]))
+
+    else:
+        rasterize_points(
+            config=config,
+            emission_types={
+            "SOx [kg]": "SO2",
+            "PM [kg]": "PM",
+            "NOx [kg]": "NOx",
+            "CO2 [kg]": "CO2",
+            "BC [kg]": "EC",
+            "ASH [kg]": "Ash",
+            "POA [kg]": "POA",
+            "CO [kg]": "CO",
+            "NMVOC [kg]": "NMVOC",
+        })
+
+
 
 @cli.command()
 @click.pass_context
