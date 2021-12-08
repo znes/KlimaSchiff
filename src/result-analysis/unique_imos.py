6c886d439fff863eff17a173f591927fc7502b29
simnh <simon.hilpert@uni-flensburg.de>
2021-12-08 10:11:12 +0100

    Add script to get unique imo numbers

---
+++ src/result-analysis/unique_imos.py
@@ -0,0 +1,31 @@
+"""
+Get set of unique imo numbers from ship routes 
+"""
+import os
+import json
+
+import pandas as pd
+
+with open("config.json") as file:
+    config = json.load(file)
+
+dirpath = os.path.join(
+    os.path.expanduser("~"),
+    config["intermediate_data"],
+    "ship_routes")
+
+files = os.listdir(
+        dirpath
+    )
+
+imos = []
+for file in files:
+    print(file)
+    imos.extend(pd.read_csv(os.path.join(dirpath, file), usecols=["imo"], dtype="int").imo.unique())
+
+unique_imos = set(imos)
+pd.Series(list(unique_imos)).to_csv(
+    os.path.join(
+        os.path.expanduser("~"),
+        config["model_data"],
+        "unique_imos.csv"))
