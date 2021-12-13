634e0a5819b02db2d521e424a061fff6ecadb5f8
simnh <simon.hilpert@uni-flensburg.de>
2021-12-13 16:17:08 +0100

    Update results anaysis script

--- src/result-analysis/analyse_emissions_by_type.py
+++ src/result-analysis/analyse_emissions_by_type.py
@@ -3,41 +3,64 @@ import pickle
 import os
 import re
 import pandas as pd
+import zipfile
 
 with open("config.json") as file:
     config = json.load(file)
 
+year = "".join([i for i in config["scenario"] if i.isdigit()])
+
 imo_by_type = os.path.join(
-    os.path.expanduser("~"), config["raw_data"], "imo_by_type.pkl"
+    os.path.expanduser("~"),
+    config["model_data"],
+    "imo_by_type_" + year + ".pkl",
 )
+
 with open(imo_by_type, "rb") as f:
     ships_per_ship_class = pickle.load(f)
 
-emissions_path = os.path.join(
-    os.path.expanduser("~"), config["intermediate_data"], "ship_emissions"
-)
+scenarios = ["2015_sq", "2030_low", "2030_high", "2040_low"]
 
-files = os.listdir(emissions_path)
-files.sort()
-
-emission_by_shiptype = {}
-for file in files:
-    df = pd.read_csv(os.path.join(emissions_path, file))
-    for k, v in ships_per_ship_class.items():
-        emission_by_shiptype[(int(re.findall("\d+", file)[0]), k)] = (
-            df.loc[df["imo"].isin(v), df.columns[7:25]].sum().values
-        )
-emissions_by_type_and_day = pd.DataFrame(
-    emission_by_shiptype, index=df.columns[7:25]
-).T
-
-emissions_by_type_and_day.to_csv(
-    os.path.join(
+for scenario in scenarios:
+    scenario_path = os.path.join(
         os.path.expanduser("~"),
-        config["result_data"],
-        "total_emissions_by_type_and_day.csv",
+        config["intermediate_data"],
+        scenario,
+        "ship_emissions",
     )
-)
+    files = os.listdir(scenario_path)
+    files.sort()
+
+    emission_by_shiptype = {}
+    for file in files:
+        if file.endswith(".zip"):
+            df = pd.read_csv(
+                os.path.join(scenario_path, file), compression="zip"
+            )
+            for k, v in ships_per_ship_class.items():
+                emission_by_shiptype[(int(re.findall("\d+", file)[0]), k)] = (
+                    df.loc[df["imo"].isin(v), df.columns[8:]].sum().values
+                )
+    emissions_by_type_and_day = pd.DataFrame(
+        emission_by_shiptype, index=df.columns[8:]
+    ).T
+
+    result_path = os.path.join(
+        os.path.expanduser("~"), config["result_data"], scenario
+    )
+
+    if not os.path.exists(result_path):
+        os.makedirs(result_path)
+
+    emissions_by_type_and_day.to_csv(
+        os.path.join(result_path, "total_emissions_by_type_and_day.csv",)
+    )
+
+# emissions_by_type_and_day["Shipclass"] = [
+#     i[1].split(' ')[0] for i in emissions_by_type_and_day.index]
+#     emissions_by_type_and_day.reset_index(inplace=True)
+#     emissions_by_type_and_day = emissions_by_type_and_day.groupby(["level_0", "Shipclass"]).sum()
+
 
 # df = pd.read_csv("total_emissions_by_type_and_day.csv", index_col=[0,1], parse_dates=True)
 # df.groupby(level=1).sum()
