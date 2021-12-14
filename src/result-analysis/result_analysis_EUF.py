58ac6548f7a5868799f3449e2802917d75dc47de
simnh <simon.hilpert@uni-flensburg.de>
2021-12-14 11:17:03 +0100

    add scenario comparison to result analysis

--- src/result-analysis/result_analysis_EUF.py
+++ src/result-analysis/result_analysis_EUF.py
@@ -52,7 +52,6 @@ pollutants = set(
     ["SOx", "NOx", "PM", "CO [kg]", "CO2", "ASH", "POA", "NMVOC", "BC"]
 )
 
-
 def correct_categories(cols):
     return [cat for col in cols for cat in pollutants if cat in col]
 
@@ -103,3 +102,59 @@ ax = df_time_agg[["NOx", "CO [kg]", "SOx"]].divide(1e9).plot(kind="bar")
 ax.set_ylabel("CO2 in Mio ton")
 ax.set_xlabel("Ship type")
 plt.savefig("figures/results/Agg_CO2_emissions_SQ.pdf")
+
+# -----------------------------------------------------------------------------
+# scenario comparison
+# ---------------------------------------------------------------------------
+scenarios = ["2015_sq", "2030_low", "2030_high", "2040_low"]
+pollutants = [
+    i + " [kg]" for i in ["SOx", "NOx", "PM", "CO", "CO2", "ASH", "POA", "NMVOC", "BC"]]
+
+d = {}
+with open("config.json") as file:
+    config = json.load(file)
+config["intermediate_data"] = "nextcloud-znes/KlimaSchiff/result_data/emissions"
+for scenario in scenarios:
+    scenario_path = os.path.join(
+        os.path.expanduser("~"),
+        config["intermediate_data"],
+        scenario,
+    )
+
+    df = pd.read_csv(os.path.join(scenario_path, "total_emissions_by_type_and_day.csv"))
+
+    # group by category ("Bulker", "Tanker" etc)
+    df["Shipclass"] = df["Unnamed: 1"].apply(lambda x: category(x))
+
+    df["Unnamed: 0"] = pd.to_datetime(df["Unnamed: 0"], format="%Y%m%d")
+    # remove 2014 data from results data set
+    df = df.set_index("Unnamed: 0")["2015"].reset_index()
+    # df.set_index(["Unnamed: 0","Unnamed: 1"], inplace=True)
+    df_sums = df.groupby(["Unnamed: 0", "Shipclass"]).sum()
+    df_sums = df_sums.sum(level=1, axis=0).T
+
+    df_sums["Pollutant"] = [row.split("-")[1] for row in df_sums.index]
+
+    d[scenario] = df_sums #.groupby("Pollutant").sum()
+
+
+pd.DataFrame({k:v.sum(axis=1) for k,v in d.items()}).to_csv(
+    "tables/annual_emissions_all_scenarios_by_engine.csv")
+pd.DataFrame({k:v.groupby("Pollutant").sum().sum(axis=1) for k,v in d.items()}).to_csv(
+    "tables/annual_emissions_all_scenarios_total.csv")
+
+heatmap = {}
+for scenario in d:
+    d[scenario].to_csv("tables/annual_emission_by_type_{}.csv".format(scenario))
+    ref = d["2015_sq"].groupby("Pollutant").sum().loc[pollutants]
+    hm = d[scenario].groupby("Pollutant").sum().loc[pollutants].div(ref.values).sub(1)
+    hm.index = [i.replace(" [kg]", "") for i in hm.index]
+    #hm = hm.round(2)
+    heatmap[scenario] = hm
+    fig, ax = plt.subplots(figsize=(10,6))
+
+    sns.heatmap(heatmap[scenario], cmap="YlGnBu_r", cbar_kws={'label': 'Deviation to SQ'},
+          annot=True, ax=ax, fmt=".2%")
+    plt.savefig(
+        "figures/results/heatmap_emission_reduction_{}.pdf".format(scenario),
+        bbox_inches="tight",)
