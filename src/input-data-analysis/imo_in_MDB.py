01c124629aa8da73c1a4a2932d3f9c9454dba37d
simnh <simon.hilpert@uni-flensburg.de>
2022-02-25 10:22:34 +0100

    Add imo in MDB analysis

---
+++ src/input-data-analysis/imo_in_MDB.py
@@ -0,0 +1,40 @@
+import os
+
+import pandas as pd
+import numpy as np
+
+from src.preprocess import create_ship_dataframe
+
+ships = create_ship_dataframe()
+
+imos = ships.IMO.unique()
+
+files = os.listdir(os.path.join(
+    os.path.expanduser("~"),
+    "klimaschiff",
+    "raw_data",
+    "processed"))
+
+l = []
+for file in files:
+    if file.endswith(".csv"):
+        df = pd.read_csv(
+            os.path.join(
+                os.path.expanduser("~"),
+                "klimaschiff",
+                "raw_data",
+                "processed",
+                file
+                )
+            )
+
+    month = df.imo.unique()
+
+    len([i for i in np.isin(month, imos) if i==True]) / len(month)
+    mask = np.isin(month, imos)
+    number, share = len(month), (1 -len(month[mask]) / len(month)) * 100
+
+    l.append((number, share))
+
+df = pd.DataFrame(l, columns=["Unique_imos", "Not in"])
+df.to_csv("imo_in_MDB_check.csv")
