491f8376a07db31a58818037548c4f974c27a8ff
simnh <simon.hilpert@uni-flensburg.de>
2022-04-21 11:30:38 +0200

    Fix analyse script

--- src/result-analysis/analyse_rasterdata.py
+++ src/result-analysis/analyse_rasterdata.py
@@ -1,25 +1,22 @@
-# import folium
-# from folium import plugins
-import geopandas as gpd
 import pandas as pd
 import os
 
-# import contextily as cx
-import matplotlib.pyplot as plt
 import xarray as xr
 import json
-import matplotlib as mpl
+
+# select scenario and run file
+scenario = "2015_sq"
 
 with open("config.json") as file:
     config = json.load(file)
 
-
 nc_dirpath = os.path.join(
-    os.path.expanduser("~"), config["result_data"], "2015_sq"
+    os.path.expanduser("~"), config["result_data"], scenario
 )
+
 ds = []
 for file in os.listdir(nc_dirpath):
-    if file.endswith("NOx.nc"):
+    if file.endswith(".nc"):
         nc_filepath = os.path.join(nc_dirpath, file)
         ds.append(
             xr.open_dataset(nc_filepath).rename({"sum": file.strip(".nc")})
@@ -33,11 +30,13 @@ for poll in ds:
             res[poll2, t.time.values] = float(t.sum().values)
 
 pd.Series(res).unstack().T.loc["2015"].to_csv(
-    "rastered_emissions_2015_as_tseries.csv"
+    os.path.expanduser("~"), config["result_data"],
+    scenario + "_rastered_emissions_as_tseries.csv"
 )
 
-#
-#
+
+# import matplotlib.pyplot as plt
+# import matplotlib as mpl
 # select.plot(
 #     cmap=mpl.cm.RdYlBu_r,
 #     norm=mpl.colors.LogNorm(
